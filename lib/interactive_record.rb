0ae5ea40339b3353d4fa0ae5b43f951f1dfba9f0
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-30 05:23:10 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -61,6 +61,11 @@ class InteractiveRecord
   end
   
   
-
+  def self.find_by(attribute_hash)
+    value = attribute_hash.values.first
+    attribute_value = value.class == Fixnum ? value : "'#{value}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{attribute_value}"
+    DB[:conn].execute(sql)
+  end
   
 end
