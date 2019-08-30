69c11d963010b497dc8c724b07a02236ec6c7c00
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-30 05:14:43 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -6,6 +6,7 @@ class InteractiveRecord
   def self.table_name
     self.to_s.downcase.pluralize
   end
+
   
   def self.column_names
     DB[:conn].results_as_hash = true
@@ -19,6 +20,12 @@ class InteractiveRecord
     end
     column_names.compact
   end
-  
+
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end  
   
 end
